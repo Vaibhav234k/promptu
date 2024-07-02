e9e57464e7c8eecd63e1d0937106fd4dc8b153da
vaibhav234k <sonivaibhav378@gmail.com>
2024-07-03 00:05:54 +0530

    added next-auth

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <>
+        <h1>Not found</h1>
+        <h1>404 page</h1>
+        </>
+    )
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,9 +10,17 @@ import {
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+  const [isSignedin, setisSignedin] = useState(false);
+  if (session && isSignedin == false) {
+    setisSignedin(true);
+  }
+  console.log(session);
   return (
     <div className="flex justify-between mr-14 mt-5">
       <h1 className="ml-8 font-bold px-4 py-2">PROMPTU</h1>
@@ -47,11 +56,23 @@ export default function Navbar() {
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <Link href="/auth" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Login
-                </NavigationMenuLink>
-              </Link>
+              {isSignedin ? (
+                <button
+                  onClick={() => {
+                    signOut();
+                  }}
+                >
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Logout
+                  </NavigationMenuLink>
+                </button>
+              ) : (
+                <button onClick={() => signIn("google")}>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Login
+                  </NavigationMenuLink>
+                </button>
+              )}
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
